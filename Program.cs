107dcc08469a4918a0758ce17c1e302a1ab1ab53
Mateus Borges <mateusbmsilva@gmail.com>
2023-02-27 13:22:23 -0300

    Aula 1

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// Aprendendo sobre o método "Trim", que faz o corte de espaços das cadeias de caracteres.
+
+string greeting = "            Hello, world!             ";
+Console.WriteLine($"[{greeting}]");
+
+// O método "TrimStart", vai fazer o corte apenas no início da cadeia de caracteres.
+string trimmedGreeting = greeting.TrimStart();
+Console.WriteLine($"[{trimmedGreeting}]");
+
+// Já o "TrimEnd", faz a mesma função do anterior, porém o corte agora é apenas no final.
+trimmedGreeting = greeting.TrimEnd();
+Console.WriteLine($"[{trimmedGreeting}]");
+
+// Por fim, o método "Trim", faz o corte tanto no início quanto no fim.
+trimmedGreeting = greeting.Trim();
+Console.WriteLine($"[{trimmedGreeting}]");
